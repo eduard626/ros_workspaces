c0222283ffa101351f14db5e307b8aa0fab73a13
Eduardo Ruiz <er13827@my.bristol.ac.uk>
2023-10-18 00:31:40 +0100

    First commit

---
+++ catkin_ws1/src/dummy_pkg1/scripts/talker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import rospy
+from std_msgs.msg import String
+import rospkg
+import os
+
+if __name__=="__main__":
+    rospy.init_node("talker")
+    rate = rospy.Rate(10)
+    pub = rospy.Publisher("topic", String, queue_size=1)
+    # get the current ws
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path("dummy_pkg1")
+    cmake_prefix = os.environ["CMAKE_PREFIX_PATH"]
+    while not rospy.is_shutdown():
+        rate.sleep()
+        pub.publish(f"Package path {pkg_path}\nPrefix {cmake_prefix}")
---
+++ readme.md
@@ -0,0 +1,5 @@
+## ROS Workspaces
+
+Experimenting with python and ros:
+* Can I have multiple ROS catkin_ws and switch within a python script?
+* Can I combine them?
